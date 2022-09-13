b516b6ecd0579f005058bbc933137aecff1878e7
jrbigmon <junioterriaga0@gmail.com>
2022-09-13 20:17:35 -0300

    Criado configuração do tailWind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    '.src/**/*.tsx',
+    './src/**/*.tsx',
+    './index.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/Fundo.svg')"
+      }
+    },
   },
   plugins: [],
 }
