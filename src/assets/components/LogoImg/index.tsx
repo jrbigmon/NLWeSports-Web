18d4d881aab899ab89834d55c7e928c49174114b
jrbigmon <junioterriaga0@gmail.com>
2022-09-13 22:22:55 -0300

    Alterado tamanho do componente logo

--- src/assets/components/LogoImg/index.tsx
+++ src/assets/components/LogoImg/index.tsx
@@ -2,7 +2,7 @@ import logoImg from '../../img/Logo.svg'
 
 export default function LogoImg() {
     return (
-        <div className="mx-auto">
+        <div className="mx-auto h-40 w-64">
             <img src={logoImg}/>
         </div>
     )
