0c69268644dafc335dac6ca2c9c07a0715d0d309
jrbigmon <junioterriaga0@gmail.com>
2022-09-14 17:25:50 -0300

    Importado e criado searchDuoBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './styles/main.css'
 import LogoImg from './assets/components/LogoImg'
 import CardsGames from './assets/components/CardsGames'
+import SearchDuoBar from './assets/components/SearchDuoBar'
 
 function App() {
   return (
     <div className="mx-auto w-auto h-auto flex flex-col align-center justify-between">
       <LogoImg />
       <CardsGames />
+      <SearchDuoBar />
     </div>
   )
 }
---
+++ src/assets/components/SearchDuoBar/index.tsx
@@ -0,0 +1,7 @@
+export default function SearchDuoBar(){
+    return (
+        <div className="h-28 bg-[#2A2634] mt-10 mx-16 py-6 px-8 border-t-4 border-t-rainbowSkyBlue">
+
+        </div>
+    )
+}
