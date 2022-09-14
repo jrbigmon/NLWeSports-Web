2e968c4b539cdc48d8495326ce80e400e7aa18dc
jrbigmon <junioterriaga0@gmail.com>
2022-09-13 22:24:04 -0300

    Alterado a margem do card para centraliza-lo

--- src/assets/components/CardGame/index.tsx
+++ src/assets/components/CardGame/index.tsx
@@ -1,6 +1,6 @@
 export default function CardGame({img}: any){
     return (
-        <div className="w-44 h-60 mx-4">
+        <div className="w-44 h-60 mx-auto">
            <img src={img} />
         </div>
     )
