ae73cfd41e386a6cd3597e439eda1271874131b0
jrbigmon <junioterriaga0@gmail.com>
2022-09-14 20:24:58 -0300

    Modificado o cartão do jogo para aplicar a caixa com gradiente e nome do game

--- src/assets/components/CardGame/index.tsx
+++ src/assets/components/CardGame/index.tsx
@@ -1,7 +1,14 @@
-export default function CardGame({img}: any){
+export default function CardGame({img, name, quantityAds}: any){
     return (
-        <div className="w-36 h-52 mx-auto">
-           <img src={img} />
+        <div className="w-40 mx-auto">
+            <a href="#" className="rounded-lg overflow-hidden">
+
+                <img src={img} className="relative w-full"/>
+                <div className="absolute bottom-0 left-0 right-0 pl-4 pb-4 bg-infoGame w-40 rounded-b-md mx-auto">
+                    <p className="text-white font-bold block">{name}</p>
+                    <p className="text-zinc-300 font-normal block">{quantityAds} anúncios</p>
+                </div>
+            </a>
         </div>
     )
 }   
