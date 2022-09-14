75f087ff523bead715008a27a0f82ff58c08c4e6
jrbigmon <junioterriaga0@gmail.com>
2022-09-14 17:25:28 -0300

    Adicionado grid de telas

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,9 +5,20 @@ module.exports = {
     './index.html'
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {
       backgroundImage: {
         galaxy: "url('/Fundo.svg')"
+      },
+      colors: {
+        rainbowPurple: '#9572FC',
+        rainbowSkyBlue: '#43E7AD',
+        raibownskyGreen: 'E2D45C'
       }
     },
   },
