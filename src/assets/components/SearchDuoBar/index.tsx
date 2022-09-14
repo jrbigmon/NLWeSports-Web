c5985f509d80e54174bb70189abc67ac4c2713f6
jrbigmon <junioterriaga0@gmail.com>
2022-09-14 20:26:01 -0300

    Adicionado os elementos na barra inferior para buscar o duo

--- src/assets/components/SearchDuoBar/index.tsx
+++ src/assets/components/SearchDuoBar/index.tsx
@@ -1,7 +1,15 @@
 export default function SearchDuoBar(){
     return (
-        <div className="h-28 bg-[#2A2634] mt-10 mx-16 py-6 px-8 border-t-4 border-t-rainbowSkyBlue">
-
+        <div className="h-28 bg-rainbowGradient mt-8 mx-16 rounded-lg flex items-end">
+            <div className="bg-[#2A2634] w-full h-[6.8rem] rounded-md py-6 px-8 flex justify-between">
+                <div>
+                    <p className="text-white font-black text-2xl">Não encontrou seu duo?</p>
+                    <p className="text-zinc-400">Publique um anúncio para encontrar novos players!</p>
+                </div>
+                <button type="button" className="text-white w-48 h-12 bg-violet-500 rounded-lg">
+                    Publicar anúncio
+                </button>
+            </div>  
         </div>
     )
 }
