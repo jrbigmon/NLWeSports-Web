380ce6e1d21de904caa6c54ab3d2d5904aa28450
jrbigmon <junioterriaga0@gmail.com>
2022-09-14 11:34:02 -0300

    Adicionado margin top ao elemento do logo

--- src/assets/components/LogoImg/index.tsx
+++ src/assets/components/LogoImg/index.tsx
@@ -2,7 +2,7 @@ import logoImg from '../../img/Logo.svg'
 
 export default function LogoImg() {
     return (
-        <div className="mx-auto h-40 w-64">
+        <div className="mx-auto h-40 w-64 mt-16">
             <img src={logoImg}/>
         </div>
     )
