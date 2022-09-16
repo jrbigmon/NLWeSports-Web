95e4fbec9fba7e2271553b551d0f9060a4b8d543
jrbigmon <junioterriaga0@gmail.com>
2022-09-16 18:56:42 -0300

    Criado interface das propriedades para o Card

--- src/assets/components/CardGame/index.tsx
+++ src/assets/components/CardGame/index.tsx
@@ -1,12 +1,18 @@
-export default function CardGame({img, name, quantityAds}: any){
+interface Game {
+    img: string
+    title: string
+    quantityAds: number
+}
+
+export default function CardGame({img, title, quantityAds}: Game){
     return (
         <div className="w-40 mx-auto">
             <a href="#" className="rounded-lg overflow-hidden">
 
-                <img src={img} className="relative w-full"/>
+                <img src={img} className="relative w-full rounded-lg"/>
                 <div className="absolute bottom-0 left-0 right-0 pl-4 pb-4 bg-infoGame w-40 rounded-b-md mx-auto">
-                    <p className="text-white font-bold block">{name}</p>
-                    <p className="text-zinc-300 font-normal block">{quantityAds} anúncios</p>
+                    <p className="text-white font-bold block">{title}</p>
+                    <p className="text-zinc-300 font-normal block">{quantityAds} anúncio(s)</p>
                 </div>
             </a>
         </div>
