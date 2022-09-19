b5d69752d3989188a90926eed8df59a92d51fe5e
jrbigmon <junioterriaga0@gmail.com>
2022-09-19 11:58:27 -0300

    Criado formulário para modal

--- src/assets/components/ModalFormBody/index.tsx
+++ src/assets/components/ModalFormBody/index.tsx
@@ -1,5 +1,7 @@
 import nlwApi from "../../../services/nlwApi"
 import {useState, useEffect} from "react"
+import * as Dialog from '@radix-ui/react-dialog';
+import { GameController } from 'phosphor-react'
 
 interface Game {
     id: string
@@ -51,35 +53,35 @@ function ModalBody() {
 
             <div className="flex justify-between mt-4 mb-2">
                 <div className="w-[48%]">
-                    <label htmlFor="weekDays" className="text-[#FFFFFF]"> Quando costuma jogar? </label>
+                    <label className="text-[#FFFFFF]"> Quando costuma jogar? </label>
                     <div className="flex mt-2">
                         <div className="mr-3">
-                            <label htmlFor="weekDays" className="block text-[#FFFFFF] font-normal">D</label>
-                            <input id="weekDays" name="weekDays" type="checkbox" value="0"  className="bg-[#18181B] w-[100%] p-2 rounded-lg text-[#71717A]"/>
+                            <label htmlFor="sunday" className="block text-[#FFFFFF] font-normal">D</label>
+                            <input id="sunday" name="weekDays" type="checkbox" value="0"  className="accent-[#18181B] w-[100%] p-2 rounded-lg text-[#71717A]"/>
                         </div>
                         <div className="mr-3">
-                            <label htmlFor="weekDays" className="block text-[#FFFFFF] font-normal">S</label>
-                            <input id="weekDays" name="weekDays" type="checkbox" value="1"  className="bg-[#18181B] w-[100%] p-2 rounded-lg text-[#71717A]"/>
+                            <label htmlFor="monday" className="block text-[#FFFFFF] font-normal">S</label>
+                            <input id="monday" name="weekDays" type="checkbox" value="1"  className="accent-[#18181B] w-[100%] p-2 rounded-lg text-[#71717A]"/>
                         </div>
                         <div className="mr-3">
-                            <label htmlFor="weekDays" className="block text-[#FFFFFF] font-normal">T</label>
-                            <input id="weekDays" name="weekDays" type="checkbox" value="2"  className="bg-[#18181B] w-[100%] p-2 rounded-lg text-[#71717A]"/>
+                            <label htmlFor="tuesday" className="block text-[#FFFFFF] font-normal">T</label>
+                            <input id="tuesday" name="weekDays" type="checkbox" value="2"  className="accent-[#18181B] w-[100%] p-2 rounded-lg text-[#71717A]"/>
                         </div>
                         <div className="mr-3">
-                            <label htmlFor="weekDays" className="block text-[#FFFFFF] font-normal">Q</label>
-                            <input id="weekDays" name="weekDays" type="checkbox" value="3"  className="bg-[#18181B] w-[100%] p-2 rounded-lg text-[#71717A]"/>
+                            <label htmlFor="wednesday" className="block text-[#FFFFFF] font-normal">Q</label>
+                            <input id="wednesday" name="weekDays" type="checkbox" value="3"  className="accent-[#18181B] w-[100%] p-2 rounded-lg text-[#71717A]"/>
                         </div>
                         <div className="mr-3">
-                            <label htmlFor="weekDays" className="block text-[#FFFFFF] font-normal">Q</label>
-                            <input id="weekDays" name="weekDays" type="checkbox" value="4"  className="bg-[#18181B] w-[100%] p-2 rounded-lg text-[#71717A]"/>
+                            <label htmlFor="thursday" className="block text-[#FFFFFF] font-normal">Q</label>
+                            <input id="thursday" name="weekDays" type="checkbox" value="4"  className="accent-[#18181B] w-[100%] p-2 rounded-lg text-[#71717A]"/>
                         </div>
                         <div className="mr-3">
-                            <label htmlFor="weekDays" className="block text-[#FFFFFF] font-normal">S</label>
-                            <input id="weekDays" name="weekDays" type="checkbox" value="5"  className="bg-[#18181B] w-[100%] p-2 rounded-lg text-[#71717A]"/>
+                            <label htmlFor="friday" className="block text-[#FFFFFF] font-normal">S</label>
+                            <input id="friday" name="weekDays" type="checkbox" value="5"  className="accent-[#18181B] w-[100%] p-2 rounded-lg text-[#71717A]"/>
                         </div>
                         <div className="mr-3">
-                            <label htmlFor="weekDays" className="block text-[#FFFFFF] font-normal">S</label>
-                            <input id="weekDays" name="weekDays" type="checkbox" value="6"  className="bg-[#18181B] w-[100%] p-2 rounded-lg text-[#71717A]"/>
+                            <label htmlFor="saturday" className="block text-[#FFFFFF] font-normal">S</label>
+                            <input id="saturday" name="weekDays" type="checkbox" value="6"  className="accent-[#18181B] w-[100%] p-2 rounded-lg text-[#71717A]"/>
                         </div>
                     </div>
                 </div> 
@@ -90,6 +92,18 @@ function ModalBody() {
                         <input id="hourEnd" name="hourEnd" type="time" placeholder="Até" className="bg-[#18181B] w-[48%] p-2 rounded-lg text-[#71717A]"/>
                     </div>
                 </div> 
+            </div>
+            <div className="mt-4">
+                <input id="useVoiceChannel" name="useVoiceChannel" value="true" type="checkbox" className="accent-[#18181B]"/>
+                <label htmlFor="useVoiceChannel" className="text-white"> Costumo me conectar ao chat de voz</label>
+            </div>
+            <div className="mt-8 flex justify-end">
+                <Dialog.DialogClose className="py-3 px-5 bg-zinc-500 rounded mr-4 text-white font-normal">
+                    Cancelar
+                </Dialog.DialogClose>
+                <button type="submit" className="text-white py-3 px-5 bg-violet-500 flex rounded">
+                    <GameController size={24} /> Encontrar duo
+                </button>
             </div>
         </form>
     )
--- src/assets/components/PublishAdModalWithSearchBar/index.tsx
+++ src/assets/components/PublishAdModalWithSearchBar/index.tsx
@@ -1,6 +1,6 @@
 import * as Dialog from '@radix-ui/react-dialog';
 import SearchDuoBar from '../SearchDuoBar';
-import ModalBody from '../ModalBody'
+import ModalBody from '../ModalFormBody'
 
 export default () => (
 
@@ -12,11 +12,11 @@ export default () => (
 
       <Dialog.Overlay className='bg-black/60 inset-0 fixed z-10'/>
 
-      <Dialog.Content className='bg-[#2A2634] w-[30vw] h-[70vh] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 fixed z-20 rounded-lg p-6'>
+      <Dialog.Content className='bg-[#2A2634] w-[30vw] h-auto top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 fixed z-20 rounded-lg p-6'>
 
-      <Dialog.Title className='text-white text-3xl font-black'> Publique um anúncio </Dialog.Title>
-      
-      <ModalBody />
+        <Dialog.Title className='text-white text-3xl font-black'> Publique um anúncio </Dialog.Title>
+        
+        <ModalBody />
 
       </Dialog.Content>
 
