46f94f17e88b86bfc37c715452150b28c4bde604
jrbigmon <junioterriaga0@gmail.com>
2022-09-19 21:03:55 -0300

    Executado a função para envio do formulário e Realizado o setState dos inputs

--- src/assets/components/ModalFormBody/index.tsx
+++ src/assets/components/ModalFormBody/index.tsx
@@ -2,6 +2,8 @@ import nlwApi from "../../../services/nlwApi"
 import {useState, useEffect} from "react"
 import * as Dialog from '@radix-ui/react-dialog';
 import { GameController } from 'phosphor-react'
+import SubmitForm from './SubmitForm'
+import CheckboxWeekDays from './CheckboxWeekDays'
 
 interface Game {
     id: string
@@ -10,7 +12,15 @@ interface Game {
 
 function ModalFormBody() {
     const [games, setGames] = useState<Game[]>([])
-
+    const [gameId, setGameId] = useState('')
+    const [name, setName] = useState('')
+    const [yearsPlaying, setYearsPlaying] = useState(0)
+    const [discord, setDiscord] = useState('')
+    const [weekDays, setWeekDays] = useState('')
+    const [hourStart, setHourStart] = useState('')
+    const [hourEnd, setHourEnd] = useState('')
+    const [useVoiceChannel, setUseVoiceChannel] = useState(false)
+    
     async function getGames() {
         const response = await nlwApi.get('/games')
         setGames( response.data )
@@ -20,15 +30,40 @@ function ModalFormBody() {
         getGames()
     }, [])
 
+    function executePostFunction(event:any) { 
+        SubmitForm(event, { 
+            gameId, 
+            name,
+            discord,
+            hourEnd,
+            hourStart, 
+            useVoiceChannel,
+            weekDays,
+            yearsPlaying 
+        })
+    }
+
     return (
-        <form className="text-sm">
+        <form 
+            className="text-sm" 
+            onSubmit={e => {
+                executePostFunction(e)
+                window.location.reload()
+            }}
+        >
             <div className="mt-8 mb-2">
                 <label htmlFor="gameId" className="text-[#FFFFFF]"> Qual o game? </label>
-                <select id="gameId" name="gameId" className="bg-[#18181B] w-[100%] p-2 mt-2 rounded-lg text-[#71717A]">
+                <select 
+                    id="gameId" name="gameId" 
+                    className="bg-[#18181B] w-[100%] p-2 mt-2 rounded-lg text-[#71717A]" 
+                    onChange={e => setGameId(e.target.value)}
+                >
                     <option className="text-[#71717A]">Selecione o game que deseja jogar</option>
                     {games.map(game => {
                         return (
-                            <option key={game.id} value={game.id} className="text-[#71717A]"> {game.title} </option>
+                            <option key={game.id} value={game.id} className="text-[#71717A]"> 
+                                {game.title} 
+                            </option>
                         )
                     })}
                 </select>
@@ -36,18 +71,39 @@ function ModalFormBody() {
                 
             <div className="mt-4 mb-2">
                 <label htmlFor="name" className="text-[#FFFFFF]"> Seu nome (ou nickname)? </label>
-                <input id="name" name="name" placeholder="Como te chamam dentro do game?" className="bg-[#18181B] w-[100%] p-2 mt-2 rounded-lg text-[#71717A]"/>
+                <input 
+                    id="name" 
+                    name="name" 
+                    placeholder="Como te chamam dentro do game?" 
+                    className="bg-[#18181B] w-[100%] p-2 mt-2 rounded-lg text-[#71717A]" 
+                    onChange={e => setName(e.target.value)}
+                />
             </div>
 
             <div className="flex justify-between mt-4 mb-2">
                 <div className="w-[48%]">
                     <label htmlFor="yearsPlaying" className="text-[#FFFFFF]"> Joga há quantos anos? </label>
-                    <input id="yearsPlaying" name="yearsPlaying" type="number" step="1" min="0" placeholder="Tudo bem ser Zero" className="bg-[#18181B] w-[100%] p-2 mt-2 rounded-lg text-[#71717A]"/>
+                    <input 
+                        id="yearsPlaying" 
+                        name="yearsPlaying" 
+                        type="number" 
+                        step="1" 
+                        min="0"
+                        placeholder="Tudo bem ser Zero" 
+                        className="bg-[#18181B] w-[100%] p-2 mt-2 rounded-lg text-[#71717A]"
+                        onChange={e => setYearsPlaying(parseInt(e.target.value))}
+                    />
                 </div>
 
                 <div className="w-[48%]">
                     <label htmlFor="discord" className="text-[#FFFFFF]"> Qual seu Discord? </label>
-                    <input id="discord" name="discord" placeholder="Usuário#0000" className="bg-[#18181B] w-[100%] p-2 mt-2 rounded-lg text-[#71717A]"/>
+                    <input 
+                        id="discord" 
+                        name="discord" 
+                        placeholder="Usuário#0000" 
+                        className="bg-[#18181B] w-[100%] p-2 mt-2 rounded-lg text-[#71717A]"
+                        onChange={e => setDiscord(e.target.value)}
+                    />
                 </div> 
             </div>
 
@@ -55,48 +111,97 @@ function ModalFormBody() {
                 <div className="w-[48%]">
                     <label className="text-[#FFFFFF]"> Quando costuma jogar? </label>
                     <div className="flex mt-2">
-                        <div className="mr-3">
-                            <label htmlFor="sunday" className="block text-[#FFFFFF] font-normal">D</label>
-                            <input id="sunday" name="weekDays" type="checkbox" value="0"  className="accent-[#18181B] w-[100%] p-2 rounded-lg text-[#71717A]"/>
-                        </div>
-                        <div className="mr-3">
-                            <label htmlFor="monday" className="block text-[#FFFFFF] font-normal">S</label>
-                            <input id="monday" name="weekDays" type="checkbox" value="1"  className="accent-[#18181B] w-[100%] p-2 rounded-lg text-[#71717A]"/>
-                        </div>
-                        <div className="mr-3">
-                            <label htmlFor="tuesday" className="block text-[#FFFFFF] font-normal">T</label>
-                            <input id="tuesday" name="weekDays" type="checkbox" value="2"  className="accent-[#18181B] w-[100%] p-2 rounded-lg text-[#71717A]"/>
-                        </div>
-                        <div className="mr-3">
-                            <label htmlFor="wednesday" className="block text-[#FFFFFF] font-normal">Q</label>
-                            <input id="wednesday" name="weekDays" type="checkbox" value="3"  className="accent-[#18181B] w-[100%] p-2 rounded-lg text-[#71717A]"/>
-                        </div>
-                        <div className="mr-3">
-                            <label htmlFor="thursday" className="block text-[#FFFFFF] font-normal">Q</label>
-                            <input id="thursday" name="weekDays" type="checkbox" value="4"  className="accent-[#18181B] w-[100%] p-2 rounded-lg text-[#71717A]"/>
-                        </div>
-                        <div className="mr-3">
-                            <label htmlFor="friday" className="block text-[#FFFFFF] font-normal">S</label>
-                            <input id="friday" name="weekDays" type="checkbox" value="5"  className="accent-[#18181B] w-[100%] p-2 rounded-lg text-[#71717A]"/>
-                        </div>
-                        <div className="mr-3">
-                            <label htmlFor="saturday" className="block text-[#FFFFFF] font-normal">S</label>
-                            <input id="saturday" name="weekDays" type="checkbox" value="6"  className="accent-[#18181B] w-[100%] p-2 rounded-lg text-[#71717A]"/>
-                        </div>
+                        <CheckboxWeekDays 
+                            htmlFor="sunday"
+                            inpurtId="sunday"
+                            textLabel="D"
+                            inputValue={"0"}
+                            setInput={setWeekDays}
+                            oldValueInput={weekDays}
+                        />
+                        <CheckboxWeekDays 
+                            htmlFor="monday"
+                            inpurtId="monday"
+                            textLabel="S"
+                            inputValue={"1"}
+                            setInput={setWeekDays}
+                            oldValueInput={weekDays}
+                        />
+                        <CheckboxWeekDays 
+                            htmlFor="tuesday"
+                            inpurtId="tuesday"
+                            textLabel="T"
+                            inputValue={"2"}
+                            setInput={setWeekDays}
+                            oldValueInput={weekDays}
+                        />
+                        <CheckboxWeekDays 
+                            htmlFor="wednesday"
+                            inpurtId="wednesday"
+                            textLabel="Q"
+                            inputValue={"3"}
+                            setInput={setWeekDays}
+                            oldValueInput={weekDays}
+                        />
+                        <CheckboxWeekDays 
+                            htmlFor="thursday"
+                            inpurtId="thursday"
+                            textLabel="Q"
+                            inputValue={"4"}
+                            setInput={setWeekDays}
+                            oldValueInput={weekDays}
+                        />
+                        <CheckboxWeekDays 
+                            htmlFor="friday"
+                            inpurtId="friday"
+                            textLabel="S"
+                            inputValue={"5"}
+                            setInput={setWeekDays}
+                            oldValueInput={weekDays}
+                        />
+                        <CheckboxWeekDays 
+                            htmlFor="saturday"
+                            inpurtId="saturday"
+                            textLabel="S"
+                            inputValue={"6"}
+                            setInput={setWeekDays}
+                            oldValueInput={weekDays}
+                        />    
                     </div>
                 </div> 
+                
                 <div className="w-[48%]">
                     <label htmlFor="hourStart" className="text-[#FFFFFF]"> Qual horário do dia? </label>
                     <div className="flex justify-between mt-2">
-                        <input id="hourStart" name="hourStart" type="time" placeholder="De" className="bg-[#18181B] w-[48%] p-2 rounded-lg text-[#71717A]"/>
-                        <input id="hourEnd" name="hourEnd" type="time" placeholder="Até" className="bg-[#18181B] w-[48%] p-2 rounded-lg text-[#71717A]"/>
+                        <input 
+                            id="hourStart" 
+                            name="hourStart" 
+                            type="time" placeholder="De" 
+                            className="bg-[#18181B] w-[48%] p-2 rounded-lg text-[#71717A]"
+                            onChange={e => setHourStart(e.target.value)}
+                        />
+                        <input 
+                            id="hourEnd" 
+                            name="hourEnd" 
+                            type="time" placeholder="Até" 
+                            className="bg-[#18181B] w-[48%] p-2 rounded-lg text-[#71717A]"
+                            onChange={e => setHourEnd(e.target.value)}
+                        />
                     </div>
                 </div> 
             </div>
+
             <div className="mt-4">
-                <input id="useVoiceChannel" name="useVoiceChannel" value="true" type="checkbox" className="accent-[#18181B]"/>
+                <input 
+                    id="useVoiceChannel" 
+                    name="useVoiceChannel"  
+                    type="checkbox" 
+                    className="accent-[#18181B]"
+                    onChange={e => e.target.checked ? setUseVoiceChannel(true) : setUseVoiceChannel(false)}
+                />
                 <label htmlFor="useVoiceChannel" className="text-white"> Costumo me conectar ao chat de voz</label>
             </div>
+
             <div className="mt-8 flex justify-end">
                 <Dialog.DialogClose className="py-3 px-5 bg-zinc-500 rounded mr-4 text-white font-normal">
                     Cancelar
