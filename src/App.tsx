7b282011de5a8dd7f28c3efd789625d27ac5c87a
jrbigmon <junioterriaga0@gmail.com>
2022-09-20 16:38:58 -0300

    Criação das rotas para renderização dos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import LogoImg from './assets/components/LogoImg'
 import CardsGames from './assets/components/CardsGames'
 import CardsAds from './assets/components/CardsAds'
 import PublishAdModalWithSearchBar from './assets/components/PublishAdModalWithSearchBar'
+import CardsAdsByGame from './assets/components/CardsAdsByGame'
+import {Route, Routes} from "react-router-dom"
 
 function App() {
   return (
     <div className="mx-auto w-auto h-auto flex flex-col">
-      <LogoImg />
-      <CardsGames />
-      <CardsAds />
-      <PublishAdModalWithSearchBar/>
+        <LogoImg />
+        <CardsGames />
+        <Routes>
+          <Route path='/' element={<CardsAds />} />
+          <Route path='/games/:id/ads' element={<CardsAdsByGame />} />
+        </Routes>
+        <PublishAdModalWithSearchBar />
     </div>
   )
 }
