f41db2d4e61558597106b538ad330d4e38e6f469
jrbigmon <junioterriaga0@gmail.com>
2022-09-20 16:40:21 -0300

    Modificado para link sem precisar dar reload na página

--- src/assets/components/CardGame/index.tsx
+++ src/assets/components/CardGame/index.tsx
@@ -1,20 +1,21 @@
+import { Link } from "react-router-dom"
 interface Game {
+    id: string
     img: string
     title: string
     quantityAds: number
 }
 
-export default function CardGame({img, title, quantityAds}: Game){
+export default function CardGame(props: Game){
     return (
         <div className="w-40 mx-auto">
-            <a href="#" className="rounded-lg overflow-hidden">
-
-                <img src={img} className="relative w-full rounded-lg"/>
+            <Link  to={`/games/${props.id}/ads`} className="rounded-lg overflow-hidden">
+                <img src={props.img} className="relative w-full rounded-lg"/>
                 <div className="absolute bottom-0 left-0 right-0 pl-4 pb-4 bg-infoGame w-40 rounded-b-md mx-auto">
-                    <p className="text-white font-bold block">{title}</p>
-                    <p className="text-zinc-300 font-normal block">{quantityAds} anúncio(s)</p>
+                    <p className="text-white font-bold block">{props.title}</p>
+                    <p className="text-zinc-300 font-normal block">{props.quantityAds} anúncio(s)</p>
                 </div>
-            </a>
+            </Link>
         </div>
     )
 }   
