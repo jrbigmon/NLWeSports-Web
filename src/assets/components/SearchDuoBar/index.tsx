aa1b9bf3730f64078f6d144d0ba4e5ad2c8e8ec0
jrbigmon <junioterriaga0@gmail.com>
2022-09-21 22:20:58 -0300

    Realizando modificações para responsividade

--- src/assets/components/SearchDuoBar/index.tsx
+++ src/assets/components/SearchDuoBar/index.tsx
@@ -5,14 +5,14 @@ export default function SearchDuoBar(){
     return (
         <div className="h-28 bg-rainbowGradient mt-8 mx-16 mb-4 rounded-lg flex items-end">
             <div className="bg-[#2A2634] w-full h-[6.8rem] rounded-md py-6 px-8 flex justify-between items-center">
-                <div>
+                <div className="xs:hidden sm:hidden md:block lg:block">
                     <p className="text-white font-black text-2xl">Não encontrou seu duo?</p>
                     <p className="text-zinc-400">Publique um anúncio para encontrar novos players!</p>
                 </div>
         
                     <Dialog.Trigger  type="button" className="inline-flex justify-between text-white w-48 h-12 bg-violet-500 hover:bg-violet-600 rounded-lg py-3 px-4">
                         <MagnifyingGlassPlus size={28} /> 
-                        Publicar anúncio
+                        Publicar <span className='xs:hidden sm:block md:block lg:block'>anúncio</span>
                     </Dialog.Trigger>
 
             </div>  
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,7 @@ module.exports = {
       sans: ['Inter', 'sans-serif']
     },
     screens: {
+      xs: '320px',
       sm: '480px',
       md: '768px',
       lg: '976px',
