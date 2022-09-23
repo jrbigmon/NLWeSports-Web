6a0ca3b297b3836c7f1c72fdc4b5bb1c40f63424
jrbigmon <junioterriaga0@gmail.com>
2022-09-23 12:18:59 -0300

    Modificação das dimensões modal publicação

--- src/assets/components/PublishAdModalWithSearchBar/index.tsx
+++ src/assets/components/PublishAdModalWithSearchBar/index.tsx
@@ -12,7 +12,7 @@ export default () => (
 
       <Dialog.Overlay className='bg-black/60 inset-0 fixed z-10'/>
 
-      <Dialog.Content className='bg-[#2A2634] w-[30vw] h-auto top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 fixed z-20 rounded-lg p-6'>
+      <Dialog.Content className='bg-[#2A2634] xs:w-[95%] sm:w-[95%] md:w-auto  h-auto top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 fixed z-20 rounded-lg p-6'>
 
         <Dialog.Title className='text-white text-3xl font-black'> Publique um anúncio </Dialog.Title>
         
